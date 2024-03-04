8ca51711577c9e8d29d65830b586272865b97f35
RoneyVaLo <rvaldelomarlopez@gmail.com>
2024-03-04 00:38:14 -0600

    add field for email and password

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -2,6 +2,8 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema(
     {
+        email: {type: String, required: true, unique: true},
+        password: {type: String, required: true},
         name: { type: String, required: true },
         username: { type: String, required: true, unique: true },
         profile_image: { type: Buffer },
